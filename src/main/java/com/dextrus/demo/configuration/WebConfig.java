67410f7dcd1e445a790d6c117550b6a830a9a457
ajaysunnydk <ajaysunnydk167@gmail.com>
2023-02-13 13:16:59 +0530

    Update WebConfig.java

--- src/main/java/com/dextrus/demo/configuration/WebConfig.java
+++ src/main/java/com/dextrus/demo/configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://.+")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT")
                 .allowedHeaders("*")
                 .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Access-Control-Max-Age", "Access-Control-Request-Headers", "Access-Control-Request-Method")
